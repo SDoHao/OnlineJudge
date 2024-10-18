81df703992ae21a903ca381a943ed2d459aabb6d
SDoHao <19951122v2@163.com>
2024-10-18 20:55:20 +0800

    Choir + Increasing and Decreasing Subsequences+some

--- Nowcoder/HJ24/main.cpp
+++ Nowcoder/HJ24/main.cpp
@@ -37,4 +37,26 @@ int main()
     }
     cout << num - _max + 1;
     return 0;
+
+    //京东 2020校招 笔试第一题：合唱队 AC:100
+    /*int num, count = 0;
+    cin >> num;
+    vector<int> data, preMax(num, 1), aftMin(num, 1);
+    for (int i = 0; i < num; i++)
+    {
+        int value;
+        cin >> value;
+        data.push_back(value);
+    }
+    preMax[0] = data[0];
+    aftMin[num - 1] = INT_MAX;
+    for (int i = 1; i < num; i++)
+        preMax[i] = max(preMax[i - 1], data[i]);
+    for (int i = num - 2; i >= 0; i--)
+        aftMin[i] = min(aftMin[i + 1], data[i + 1]);
+    for (int i = 0; i < num; i++)
+        if(preMax[i] > aftMin[i])
+            count++;
+    cout << count << endl;
+    return 0;*/
 }
